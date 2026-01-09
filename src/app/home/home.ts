0d87c5bdc957d7a5a1b2da919a6b471f49318920
Peti0u <quent1.vibrac@gmail.com>
2026-01-09 22:47:41 +0100

    Home Nav Animations

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,4 +7,19 @@ import { Router } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  active_bar: HTMLElement | null = null; //on definit un elmt actif (dans la nav)
+  @ViewChild('nav_dashboard') navHomeRef!: ElementRef; //On recupere le premier elmt actif (home)
+
+  ngAfterViewInit() {
+    this.active_bar = this.navHomeRef.nativeElement; //On l'assigne après l'initialisation
+  }
+
+  RendActif(div: HTMLElement) {
+    if (this.active_bar != null) {
+      this.active_bar.style.backgroundColor = 'transparent';
+    }
+    this.active_bar = div;
+    div.style.backgroundColor = '#ededed60';
+  }
+}
